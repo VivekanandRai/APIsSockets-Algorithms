025be2a7cba632490bf5570267652ce498e238dd
VivekanandRai <147989381+VivekanandRai@users.noreply.github.com>
2026-03-31 01:07:36 +0530

    Time: 25 ms (68.87%) | Memory: 83.1 MB (65.07%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,28 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        sort(nums ,0 , nums.length-1);
         return nums;
+    }
+
+    public static void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    // Merge subarrays: arr[l..m] and arr[m+1..r]
+    public static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1, n2 = r - m;
+        int[] L = new int[n1], R = new int[n2];
+        System.arraycopy(arr, l, L, 0, n1);
+        System.arraycopy(arr, m + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
     }
 }
