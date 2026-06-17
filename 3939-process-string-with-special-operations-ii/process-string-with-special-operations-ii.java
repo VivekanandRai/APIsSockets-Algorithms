18a09bbf31396924bf2fdf33bbcc7c0e95b6f4c3
VivekanandRai <147989381+VivekanandRai@users.noreply.github.com>
2026-06-18 01:04:14 +0530

    Time: 44 ms (11.54%) | Memory: 47.7 MB (88.46%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public char processStr(String s, long k) {
+        long sbl=0;
+
+        for(char c:s.toCharArray()){
+            if(Character.isLowerCase(c)) sbl++;
+            else if(c=='#') sbl*=2;
+            else if(c=='*'){
+                if(sbl>0) sbl--;
+            }
+        }
+        Character def='.';
+
+        if(k>=sbl) return def;
+
+        for(int i=s.length()-1;i>=0;i--){
+            char c= s.charAt(i);
+
+            if(c=='*'){
+                sbl++;
+            }else if(c=='%'){
+                k= sbl-k-1;
+            }else if(c=='#'){
+                long half= sbl/2;
+                if(k>=half){
+                    k=k-half;
+                }
+                sbl/=2;
+            }else{
+                if(k==sbl-1) return c;
+                else sbl--;
+            }
+        }
+        return def;
+
+    }
+}
