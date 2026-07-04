a3e71b8a6a3c03135d1e439425d8a63a92e50c05
VivekanandRai <147989381+VivekanandRai@users.noreply.github.com>
2026-07-04 14:59:59 +0530

    Time: 13 ms (57.07%) | Memory: 47.1 MB (30.27%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[][] dir= {{0,1},{1,0},{0,-1},{-1,0}};
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int m= grid.size();
+        int n=grid.get(0).size();
+
+        int[][] hmat = new int[m][n];
+        for(int[] h : hmat) Arrays.fill(h , Integer.MIN_VALUE);
+
+        var pq= new PriorityQueue<int[]>((a,b)->b[2]-a[2]);
+
+        if(grid.get(0).get(0)==1) health--;
+
+        pq.add(new int[]{0,0,health});
+        while(!pq.isEmpty()){
+            int[] curr= pq.poll();
+            int x= curr[0];
+            int y= curr[1];
+            int h= curr[2];
+
+            if(h<1) continue;
+            if(x==m-1 && y==n-1 && hmat[x][y]>0) return true;
+
+            for( int[] d :dir){
+                int nx= x+d[0];
+                int ny=y+d[1];
+                int nh=h;
+                
+                if(nx>=0 && ny>=0 && nx< m && ny < n && hmat[nx][ny]< nh ){
+                    if(grid.get(nx).get(ny)==1) nh--;
+
+                    if(hmat[nx][ny]>= nh) continue;
+
+                    hmat[nx][ny]= Math.max(hmat[nx][ny] , nh);
+                    pq.add(new int[]{nx, ny , hmat[nx][ny]});
+
+                }
+            }
+        }
+        return false;
+
+    }
+}
